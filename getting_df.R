4093567a00e5c80d93f591e37b42811d1b5eb6e4
William Whedon <wgw5@duke.edu>
2016-05-03 21:00:49 -0400

    nba data frame

--- getting_df.R
+++ getting_df.R
@@ -1,27 +1,30 @@
 library(rvest)
+library(dplyr)
 
+#
 base_url = "http://espn.go.com/nba/standings/_/season/"
 year = 2016:2003
 yr_url = sapply(seq_along(year), function(x) (paste0(base_url, year[x])))
 
-
-#read_url = sapply(seq_along(year), function(x) read_html(yr_url[x]))
-try = read_html(yr_url[1])
-teams = try %>% html_nodes(".team-names") %>% html_text()
-wins = try %>% html_nodes(".team+ td")%>% html_text() %>% as.numeric()
-loss = try %>% html_nodes("td:nth-child(3)") %>% html_text() %>% as.numeric()
-ppg = try %>% html_nodes("td:nth-child(10)") %>% html_text() %>% as.numeric()
-opg = try %>% html_nodes("td:nth-child(11)") %>% html_text() %>% as.numeric()
+htmls = lapply(seq_along(yr_url), function(x) read_html(yr_url[x]))
 
 NBA = lapply(seq_along(year), function(x) data.frame(
-  Season = rep(paste0(year[x]-1, "-",year[x]), length(read_html(yr_url[x])%>% html_nodes(".team-names") %>% html_text())),
-  Team =  read_html(yr_url[x])%>% html_nodes(".team-names") %>% html_text(),
-  Wins = read_html(yr_url[x]) %>% html_nodes(".team+ td")%>% html_text() %>% as.numeric(),
-  Losses = read_html(yr_url[x]) %>% html_nodes("td:nth-child(3)") %>% html_text() %>% as.numeric(),
-  PPG = read_html(yr_url[x]) %>% html_nodes("td:nth-child(10)") %>% html_text() %>% as.numeric(),
-  Opp_PPG = read_html(yr_url[x]) %>% html_nodes("td:nth-child(11)") %>% html_text() %>% as.numeric(),
+  Season = rep(paste0(year[x]-1, "-",year[x]), length(htmls[[x]]%>% html_nodes(".team-names") %>% html_text())),
+  Team =  htmls[[x]] %>% html_nodes(".team-names") %>% html_text(),
+  Wins = htmls[[x]] %>% html_nodes(".team+ td")%>% html_text() %>% as.numeric(),
+  Losses = htmls[[x]] %>% html_nodes("td:nth-child(3)") %>% html_text() %>% as.numeric(),
+  PPG = htmls[[x]] %>% html_nodes("td:nth-child(10)") %>% html_text() %>% as.numeric(),
+  Opp_PPG = htmls[[x]] %>% html_nodes("td:nth-child(11)") %>% html_text() %>% as.numeric(),
   
   stringsAsFactors = FALSE
 ))
-df = do.call(rbind, NBA)
-View(df)
+
+NBA.df = do.call(rbind, NBA)
+View(NBA.df)
+
+NBA.df = NBA.df %>% mutate(Exp_W1 = round((NBA.df$PPG^13.91/(NBA.df$PPG^13.91 + NBA.df$Opp_PPG^13.91))*(NBA.df$Wins+NBA.df$Losses)),
+                         Exp_L1 = (NBA.df$Wins + NBA.df$Losses) - round((NBA.df$PPG^13.91/(NBA.df$PPG^13.91 + NBA.df$Opp_PPG^13.91))*(NBA.df$Wins+NBA.df$Losses)),
+                         Exp_W2 = round((NBA.df$PPG^16.5/(NBA.df$PPG^16.5 + NBA.df$Opp_PPG^16.5))*(NBA.df$Wins + NBA.df$Losses)),
+                         Exp_L2 = (NBA.df$Wins + NBA.df$Losses)- round((NBA.df$PPG^16.5/(NBA.df$PPG^16.5 + NBA.df$Opp_PPG^16.5))*(NBA.df$Wins + NBA.df$Losses)))
+NBA.df = NBA.df %>% mutate(err1 = abs(NBA.df$Wins - NBA.df$Exp_W1), err2 = abs(NBA.df$Wins - NBA.df$Exp_W2))
+View(NBA.df)

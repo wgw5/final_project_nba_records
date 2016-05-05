7ebc500c141899dcf14ed4b726d4642d7ba948d5
William Whedon <wgw5@duke.edu>
2016-05-05 11:58:26 -0400

    commented shiny app

--- new_shiny.R
+++ new_shiny.R
@@ -8,20 +8,27 @@ shinyApp(
       "NBA Records"
     ),
     sidebarPanel(
+      #choose season you want to look at
       selectInput("year", "Season:", sort(unique(NBA.df$Season), decreasing = TRUE), selected = "2015-2016"),
       hr(),
+      #which team based on the season
       uiOutput("choose_team"), #dependent on country chosen
       hr(),
+      #option of algorithm you look at, or choose both
       selectInput("method", "Method:", c("Morey's Method", "Hollinger's Method", "Both Methods")) #dependent on city chosen
       
       
     ),
     mainPanel(
-      h4("How'd your team do?"),
+      h3("How'd your team do?"),
+      #plot the comparison of observed vs expected wins
       plotOutput("comparison"),
       br(),
+      #output a message of how the team did based on expectations
       textOutput("mess"),
       br(),
+      h4("Average Error of Algorithm"),
+      #plot to show avg amount of games the algorithm was off by for a team in that season
       plotOutput("err")
     )
     
@@ -29,12 +36,12 @@ shinyApp(
   ),
   
   server = function(input, output, session) {
-    #can only choose a city from the chosen country
+    #can only choose a team that was from chosen season
     output$choose_team = renderUI({
       selectInput("team", "Teams:", sort(NBA.df$Team[NBA.df$Season == input$year]))
     })
     
-    #update forecast function
+    #function returning vector of observed wins and expected wins based on season, team, and method
     get_method = reactive({
       if(input$method == "Morey's Method"){
         return(c(NBA.df$Wins[NBA.df$Season == input$year & NBA.df$Team == input$team],
@@ -50,7 +57,7 @@ shinyApp(
                  NBA.df$Exp_W2[NBA.df$Season == input$year & NBA.df$Team == input$team]))
       }
     })
-    
+    #function to get bar labels for graph
     titles = reactive({
       if(input$method == "Both Methods"){
         return(c("Actual Wins", "Morey's Expected", "Hollinger's Expected"))
@@ -60,14 +67,19 @@ shinyApp(
       }
     })
     
+    #first plot, putting observed wins next to expected
+    #two bars if one method chosen, 3 if both methods chosen
     output$comparison = renderPlot({
       barplot(get_method(), names.arg = titles(), ylab = "Number of Wins")
     })
     
+    #output a message
+    #needs to be updated
     output$mess = renderText({
       get_method()
     })
     
+    #vector of colors(all black), will make chosen season blue to highlight it in bottom graph
     colors = reactive({
       temp = rep("black", length(mor_err))
       yrs = sort(unique(NBA.df$Season))
@@ -76,6 +88,7 @@ shinyApp(
       return(temp)
     })
     
+    #plot the avg error of number of games off for chosen algorithm by season
     output$err = renderPlot({
       if(input$method == "Morey's Method"){
         barplot(mor_err, names.arg = sort(unique(NBA.df$Season)), las = 2,  ylab = "Avg. Games Wrong", main = "Morey's Method", col = colors())
